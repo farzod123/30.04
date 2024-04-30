20da72d5e622132c82a274c929159e6748d722a3
BFF <furkat@MacBook-Air-Furkat-2.local>
2024-04-30 13:27:52 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,150 @@
+// let password = ''
+
+// do {
+//     password = +prompt('any password')
+// } while (password !== 2224);
+
+// console.log('correct', password);
+
+
+
+/* dz */
+
+// let alphabet = "ABSDEFGHIJKLMNOPQRSTUVWXYZ"
+
+// let vowels = []
+// let consonants = []
+
+// for (let i = 0; i < alphabet.length; i++) {
+//     const letter = alphabet[i];
+//     if ("AEUOI".indexOf(letter) == -1) {
+//         consonants.push(letter)
+//     } else {
+//         vowels.push(letter)
+//     }
+// }
+
+// console.log(vowels);
+// console.log(consonants);
+
+
+
+let arr = [
+    {
+        id: Math.random(),
+        name: 'Timur',
+        info: {
+            school: '235',
+            faculity: 'SMM'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Imran',
+        info: {
+            school: 'ne izvestno',
+            faculity: 'programming'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Aminjon',
+        info: {
+            school: '444',
+            faculity: 'Dizayn'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Maxmud',
+        info: {
+            school: '777',
+            faculity: '3dsmax'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Muxammad',
+        info: {
+            school: '5555',
+            faculity: 'Backend'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Timur',
+        info: {
+            school: '235',
+            faculity: 'SMM'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Imran',
+        info: {
+            school: 'ne izvestno',
+            faculity: 'programming'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Aminjon',
+        info: {
+            school: '444',
+            faculity: 'Dizayn'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Maxmud',
+        info: {
+            school: '777',
+            faculity: '3dsmax'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Maxmud',
+        info: {
+            school: '777',
+            faculity: '3dsmax'
+        },
+    },
+    {
+        id: Math.random(),
+        name: 'Muxammad',
+        info: {
+            school: '5555',
+            faculity: 'Backend'
+        },
+    },
+]
+
+let categories = [
+    {
+        course: ' SMM',
+        count: 0
+    },
+    {
+        course: 'PROGRAMMING',
+        count: 0
+    },
+    {
+        course: '     3DSMAX',
+        count: 0
+    },
+    {
+        course: ' DIZAYN',
+        count: 0
+    },
+    {       
+        course: '   BACKEND',
+        count: 0
+    },
+]
+// arr.forEach((faculity) =>{})
+// categories.forEach((couses) =>{})
+let count = ''
+while (faculity) {
+    
+}
